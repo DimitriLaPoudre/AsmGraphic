c8d4a655ad83f8e5a080124a8d8c4767d29e64b7
DimitriLaPoudre <lou.pellegrino@epitech.eu>
2025-06-19 12:45:52 +0200

    [=] Clean basic_game

--- examples/basic_game/src/analyze_event.c
+++ examples/basic_game/src/analyze_event.c
@@ -4,13 +4,10 @@ void analyze_event(AsmEvent event, game_t *game)
 {
     switch (event.info.type) {
         case AsmEventKeyPressed:
-            AsmPrint("Key pressed: %b\n", event.key.keycode);
             break;
         case AsmEventKeyRelease:
-            AsmPrint("Key release: %b\n", event.key.keycode);
             break;
         case AsmEventMouseButtonPressed:
-            AsmPrint("Button pressed: %b at x: %w, y: %w\n", event.mouse.button, event.mouse.pos_window.x, event.mouse.pos_window.y);
             if (event.mouse.button == 1
             && (event.mouse.pos_window.x - 400) * (event.mouse.pos_window.x - 400)
             + (event.mouse.pos_window.y - 300) * (event.mouse.pos_window.y - 300) <= 50 * 50) {
@@ -23,10 +20,8 @@ void analyze_event(AsmEvent event, game_t *game)
             }
             break;
         case AsmEventMouseMoved:
-            AsmPrint("Mouse moved at x: %w, y: %w\n", event.mouse.pos_window.x, event.mouse.pos_window.y);
             break;
         case AsmEventMouseButtonRelease:
-            AsmPrint("Button release: %b\n", event.mouse.button);
             if (game->clicked_state == 1 && event.mouse.button == 1) {
                 game->clicked_state = 0;
                 AsmSetColorCircle(game->target, AsmGreen);
@@ -34,17 +29,10 @@ void analyze_event(AsmEvent event, game_t *game)
             }
             break;
         case AsmEventGetFocus:
-            AsmPutlstr("Window get focus");
             break;
         case AsmEventLostFocus:
-            AsmPutlstr("Window lost focus");
             break;
         case AsmEventWindowModified:
-            AsmPutlstr("Window modified");
-            if (AsmHasMovedWindow(game->window))
-                AsmPutlstr("Window moved");
-            if (AsmHasResizedWindow(game->window))
-                AsmPutlstr("Window resized");
             break;
         case AsmEventClose:
             if (AsmCloseWindow(game->window))
